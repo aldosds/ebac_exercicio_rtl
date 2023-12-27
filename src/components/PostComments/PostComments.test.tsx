cf3af06ab7f9f885e5cfd3f75852fffd5dcba936
Aldo Santos <aldoacesso@gmail.com>
2023-12-27 12:17:44 -0300

    Testado a inserção de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -4,7 +4,29 @@ import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<Post/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Verificando a inserção de um comentários', () => {
+        render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Que legal! Gostei'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        expect(screen.getByText('Que legal! Gostei')).toBeInTheDocument()
+    })
+    
+    it('Verificando a inserção de mais um comentários', () => {
+        render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('campo-comment'), {
+            target: {
+                value: 'Quero o meu kkkk'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        expect(screen.getByText('Quero o meu kkkk')).toBeInTheDocument()
+    })
 });
